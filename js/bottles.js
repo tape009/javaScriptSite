fceecb2c5ec28cf1fe1421fbeca9fd837e651e39
tape009 <77396321+tape009@users.noreply.github.com>
2022-01-25 11:37:05 -0500

    Update bottles.js

--- js/bottles.js
+++ js/bottles.js
@@ -2,7 +2,7 @@ var words = "pop";
 var count = 99;
 while (count > 0 ){
     document.write(count + " "+ words + " of pop  on the wall <br>");
-    document.write(count + " "+ words + "of pop <br>")
+    document.write(count + " "+ words + " of pop <br>")
     document.write("Take one down, pass it around <br>")
     count = count - 1
     if(count  >0 )
