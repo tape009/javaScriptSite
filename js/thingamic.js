6dcf54a551f412deff26fb3bcf38d6e36eee719f
ALice Davis <christopherdav74085@gmail.com>
2022-02-07 13:16:38 -0500

    added week 3 and the commted for the thingamajiga

---
+++ js/thingamic.js
@@ -0,0 +1,41 @@
+/**
+ *
+ * if the number that is input in size is zero it just display clank and nothing eles
+ * thunk is display only if size is 1 wich can only happen if it start off as 1. 
+ * when size is greater then 1 facky is times by size and it become the number size is 
+ * after this it call clunk and facky is put in as time and it display the word clunk  that amount of times
+ * so when it gose back it time facky by itself makeing it exponential and it dose this as many time as size 
+ * also when something get display it add 1 to the clunk counter wich is display when size reaches 0 
+ *  
+ */
+function clunk(times) {
+    var num = times;
+    while (num > 0) {
+    display("clunk");
+    num = num - 1;
+    }
+   }
+   function thingamajig(size) {
+    var facky = 1;
+    clunkCounter = 0;
+    if (size == 0) {
+    display("clank");
+    } else if (size == 1) {
+    display("thunk");
+    } else {
+    while (size > 1) {
+    facky = facky * size;
+    size = size - 1;
+    }
+    clunk(facky);
+    }
+   }
+   function display(output) {
+    console.log(output);
+    document.writeln(output+"<br>");
+    clunkCounter = clunkCounter + 1;
+   }
+   var clunkCounter = 0;
+   thingamajig(1);
+   console.log(clunkCounter);
+   document.writeln(clunkCounter);
