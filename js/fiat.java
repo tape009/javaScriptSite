1558141327158aa0d89e43f9fd4d604c18265695
ALice Davis <christopherdav74085@gmail.com>
2022-03-02 14:24:44 -0500

    add the fiat project to my site

---
+++ js/fiat.java
@@ -0,0 +1,31 @@
+var fiat = {
+    make: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "Medium Blue",
+    passengers: 2,
+    convertible: false,
+    mileage: 88000,
+    started: false,
+
+    start: function() 
+    {
+    this.started = true;
+        },
+        stop: function() 
+        {
+        this.started = false;
+        },
+            drive: function() 
+            {
+            if (this.started)
+            {
+            alert("Zoom zoom!");
+            } 
+                else
+                {
+                alert("You need to start the engine first.");
+                }
+                }
+}
+ 
